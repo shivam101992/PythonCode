8dd4b2f3aeefee800df1afaffb4e855fdb1d7439
shivam101992 <shivam.lyflock@gmail.com>
2016-11-06 01:00:53 -0800

    all subsets of a set and sum of their elements

---
+++ Subset_Sum.py
@@ -0,0 +1,23 @@
+def subsets(nums):
+  if nums is None: return None
+  subsets = [[]] 
+  next = [] 
+  for n in nums:
+    for s in subsets:
+      next.append(s + [n])
+    subsets += next
+    next = []
+  return subsets
+  
+def check(t):
+    final = list()
+    subset = list()
+    subset = subsets(t)
+    for item in subset:
+        final.append(sum(item))
+    print ' '.join(str(x) for x in sorted(final))
+n = int(raw_input())
+for i in range(0,n):
+    t = int(raw_input())
+    arr = [int(x) for x in raw_input().split()]
+    check(arr)
