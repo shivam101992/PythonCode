582cc9cce6c352c9690ac0fc7e3e6c457623a6bd
shivam101992 <shivam.lyflock@gmail.com>
2016-12-24 23:22:24 -0800

    Implementation of expression trees
    Along with error handling for missing parentheses

--- TreeImplementation.py
+++ TreeImplementation.py
@@ -42,6 +42,55 @@ def printTraversals():
 	print
 	printPreOrderTraversal(tree)
 
+def getTokenlist(expr):
+	l = list(expr)
+	new_list = []
+	for value in l:
+	    if value.isdigit():
+	    	new_list.append(int(value))
+	    else:
+	    	new_list.append(value)	
+	new_list.append('end')    	
+	return new_list
+
+def get_token(tokenList, expected):
+	if tokenList[0] == expected:
+		del tokenList[0]
+		return True
+	else:
+		return False	
+
+def get_number(tokenList):
+	if get_token(tokenList,'('):
+		x = get_sum(tokenList)
+		if not get_token(tokenList,')'):
+			raise 'Missing Parentheses'
+		return x
+	else:
+		x = tokenList[0]
+		if type(x)!=type(0): return None
+		tokenList[0:1] = []
+		return Tree(x,None,None)	
+
+def get_sum(tokenList):
+	a = get_product(tokenList)
+	if get_token(tokenList,'+'):
+		b = get_sum(tokenList)
+		return Tree('+',a,b)
+	else:
+		return a	
+
+def get_product(tokenList):
+	a = get_number(tokenList)
+	if get_token(tokenList,'*'):
+		b = get_product(tokenList)
+		return Tree('*',a,b)
+	else:
+		return a		
 #Function Calls	
 getTotalOfExpression()
 printTraversals()
+token_list = getTokenlist("(3+7)*9")
+token_list1 = [9, '*', '(', 11, '+', 5, ')', '*', 7, 'end']
+tree = get_sum(token_list1)
+printPostOrderTraversal(tree)
