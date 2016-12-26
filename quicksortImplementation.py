e842594a055f62dd415935365ef42a8af921ecd5
shivam101992 <shivam.lyflock@gmail.com>
2016-12-26 02:24:49 -0800

    Quick Sort Algorithm Implementation
     In Place implementation of Quick Sort

---
+++ quicksortImplementation.py
@@ -0,0 +1,33 @@
+def quickSort(t, start, end):
+	if start < end:
+		pivot = partition(t, start, end)
+		quickSort(t, start, pivot-1)
+		quickSort(t, pivot+1, end)
+	return t
+
+def partition(t, start, end):
+	pivot = t[start]
+	done = False
+	left = start + 1
+	right = end
+	while not done:
+		while t[left] <= pivot and left <= right:
+			left += 1
+		while t[right] >= pivot and right >= left:
+			right -= 1		
+		if right < left:
+			done = True
+		else:
+			temp = t[right]
+			t[right] = t[left]
+			t[left] = temp
+	temp = t[start]
+	t[start] = t[right]
+	t[right] = temp
+	return right
+
+if __name__=="__main__":
+	t = [int(x) for x in raw_input().split()]
+	s = 0
+	e = len(t)-1
+	print quickSort(t,s,e)				
